2797ad0d55f02493120ba32639cc6caee5f738d4
JFidddy <jrfidacaro@gmail.com>
2026-09-02 13:16:44 -0400

    lab1.4

--- task_1/task_1/my_pub.py
+++ task_1/task_1/my_pub.py
@@ -7,16 +7,18 @@ class Time(Node):
     def __init__(self):
         super().__init__('time_pub')
         self.publisher_ = self.create_publisher(String, 'my_first_topic', 10)
-        timer_period = 1  # seconds
+        timer_period = 5  # seconds
         self.timer = self.create_timer(timer_period, self.time_call)
-        self.t = 0
+        self.i = 0
+        self.start_time = self.get_clock.now()
     
     def time_call(self): 
         msg = String()
-        msg.data = "Time Alive: %d" % self.t
+        time_alive = self.get_clock.now() - self.start_time
+        msg.data = "Time Alive: %d" % time_alive
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.t += 1
+        self.i += 1
 
 def main(args=None):
     rclpy.init(args=args)
@@ -26,7 +28,7 @@ def main(args=None):
     rclpy.spin(time_pub)
     time_pub.destroy_node()
     rclpy.shutdown()
-    
+
 if __name__ == '__main__':
     main()
 
